f7bcb1eb63d0ea083e3c3d21fa9d39e2df64790c
Mads <mads.soenderstrup@gmail.com>
2022-11-07 11:16:42 +0100

    arch opg1 & 2

--- 21_Associations_p2/src/opg4/Kollegie.java
+++ 21_Associations_p2/src/opg4/Kollegie.java
@@ -1,5 +1,6 @@
 package opg4;
 
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class Kollegie {
@@ -51,5 +52,36 @@ public class Kollegie {
             boliger.remove(bolig);
             bolig.setKollegie(null);
         }
+    }
+
+    public int getAntalLejerAftaler() {
+        int antalLejerAftaler = 0;
+
+        for (Bolig b : boliger) {
+            antalLejerAftaler += b.getLejeaftaler().size();
+        }
+
+        return antalLejerAftaler;
+    }
+
+    public double gennemsnitligAntalDage() {
+        int sum = 0;
+        int antal = 0;
+
+        for (Bolig b : boliger) {
+            for (Lejeaftale l : b.getLejeaftaler()) {
+                if (l.getTilDato() != null) {
+                    sum += ChronoUnit.DAYS.between(l.getFraDato(), l.getTilDato());
+                    antal++;
+                }
+            }
+        }
+
+        double avg = 0.0;
+        if (antal > 0) {
+            avg = (double) sum / antal;
+        }
+
+        return avg;
     }
 }
--- 21_Associations_p2/src/opg4/Lejeaftale.java
+++ 21_Associations_p2/src/opg4/Lejeaftale.java
@@ -6,6 +6,8 @@ public class Lejeaftale {
     private LocalDate fraDato;
     private LocalDate tilDato;
     private Bolig bolig;
+    private Lejer lejer1;
+    private Lejer lejer2;
 
     public Lejeaftale(Bolig bolig) {
         fraDato = LocalDate.now();
@@ -30,5 +32,25 @@ public class Lejeaftale {
 
     public Bolig getBolig() {
         return bolig;
+    }
+
+    public Lejer getLejer1() {
+        return lejer1;
+    }
+
+    public void setLejer1(Lejer lejer) {
+        if (lejer1 != lejer) {
+            lejer1 = lejer;
+        }
+    }
+
+    public Lejer getLejer2() {
+        return lejer2;
+    }
+
+    public void setLejer2(Lejer lejer) {
+        if (lejer2 != lejer) {
+            lejer2 = lejer;
+        }
     }
 }
